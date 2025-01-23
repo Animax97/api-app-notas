7e73ca144fdb7a363f00577b49cae88fac1345e4
Jonathan Tito Gira <jonathantitogira@librodepases.com>
2025-01-23 14:52:29 -0300

    .

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/create', (req, res) => {
-    console.log('Ruta de Creación de Usuario');
+    res.send('Ruta de Creación de Usuario')
 });
 
 export default router;
